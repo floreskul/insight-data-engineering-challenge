8def24e1cdf7675422ad973a6e43990d872e5c0f
Volodymyr Floreskul <volodymyr@floreskul.name>
2015-03-22 18:12:11 +0100

    Added more description about the tokenizer

--- insight_challenge/tokenize.py
+++ insight_challenge/tokenize.py
@@ -7,11 +7,14 @@ _tokenizer = nltk.tokenize.WordPunctTokenizer()
 
 
 def word_tokenize(text, tokenizer=_tokenizer):
+    '''Split the given text into tokens'''
+
     # First remove all hyphens and apostrophes from the text
     # We can do it safely as they aren't used to separate different words
     text = text.replace('-', '')
     text = text.replace('\'', '')
 
     tokens = tokenizer.tokenize(text)
+    # Remove tokens that are just punctuation symbols
     words = filter(lambda x: x not in string.punctuation, tokens)
     return map(str.lower, words)
