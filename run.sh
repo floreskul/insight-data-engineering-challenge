ce2703be8ebf56ec960cf0510551ffd50991a34f
Volodymyr Floreskul <volodymyr@floreskul.name>
2015-03-23 00:34:44 +0100

    Fixed dependencies checking conditions in run.sh

--- run.sh
+++ run.sh
@@ -2,21 +2,21 @@
 
 # Install dependencies
 if [ "$(uname)" == "Darwin" ]; then
-    if [! python3 -V > /dev/null 2>&1 ]; then
+    if ! type python3 > /dev/null; then
         # Install Homebrew
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         # Install Python3, versions >= 3.4 include pip3 by default
         brew install python3
     fi
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    if [! python3 -V > /dev/null 2>&1 ]; then
+    if ! type python3 > /dev/null; then
         apt-get install -y python3
     fi
-    if [! pip3 -V > /dev/null 2>&1 ]; then
+    if ! type pip3 > /dev/null; then
         apt-get install -y python3-pip
     fi
 fi
-if [! python -c "import nltk" > /dev/null 2>&1 ]; then
+if ! python3 -c "import nltk" > /dev/null; then
     pip3 install nltk
 fi
 
